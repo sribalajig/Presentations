9c54d4675e5cbfd98d368de34b8b17cdf867a17c
Sri Balaji Gopinath <balaji@babajob.com>
2016-10-09 23:00:38 +0530

    Adding custom time formatting

--- prezi.core/model/presentation.go
+++ prezi.core/model/presentation.go
@@ -1,12 +1,45 @@
 package model
 
 import (
-//"time"
+	"gopkg.in/mgo.v2/bson"
+	"log"
+	"time"
 )
 
 type Presentation struct {
-	Id        string
-	Title     string
-	ThumbNail string
-	CreatedAt string
+	Id        string    `bson:"id" json:"id"`
+	Title     string    `bson:"title" json:"title"`
+	ThumbNail string    `bson:"thumbnail" json:"thumbNail"`
+	CreatedAt time.Time `bson:"createdAt" json:"createdAt"`
+}
+
+/*This is to handle the custom time format*/
+func (presentation *Presentation) SetBSON(raw bson.Raw) error {
+	result := new(struct {
+		Id        string `bson:"id" json:"id"`
+		Title     string `bson:"title" json:"title"`
+		ThumbNail string `bson:"thumbnail" json:"thumbNail"`
+		CreatedAt string `bson:"createdAt" json:"createdAt"`
+	})
+
+	err := raw.Unmarshal(result)
+
+	if err != nil {
+		return err
+	}
+
+	presentation.Id = result.Id
+	presentation.Title = result.Title
+	presentation.ThumbNail = result.ThumbNail
+
+	form := "January _2, 2006"
+	t, e := time.Parse(form, result.CreatedAt)
+
+	if e != nil {
+		log.Println(e.Error())
+	}
+
+	presentation.CreatedAt = t
+
+	return nil
 }
